c58c1dfadc85d2198d2675320ead6dedf732a133
Vanja Cvetojevic <vanja@Vanjas-MacBook-Pro.local>
2017-08-29 23:45:43 +0200

    Re-added socket.io, fixed stuff in app. Now connection to the socket is working.

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
